2c8a9bb76ce248de6b53c6643c44241b2b3beca0
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-03-26 17:06:25 +0900

    Add files via upload

--- m) data_dst Auto White Balance.bat
+++ m) data_dst Auto White Balance.bat
@@ -6,6 +6,7 @@ mkdir "%WORKSPACE%\data_dst" 2>nul
 "%PYTHON_EXECUTABLE%" "%DFL_ROOT%\main.py" videoed extract-video ^
     --input-file "%WORKSPACE%\data_dst.*" ^
     --output-dir "%WORKSPACE%\data_dst" ^
+    --output-ext "png" ^
     --fps 0
 
 "%PYTHON_EXECUTABLE%" "%INTERNAL%\faceutil\awb.py"
