2a4c9a99fea4b0bf1be506713350be4b6c1e951d
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-03-27 19:01:52 +0900

    Add files via upload

--- 4) data_src extract faces S3FD best GPU.bat
+++ 4) data_src extract faces S3FD best GPU.bat
@@ -5,9 +5,9 @@ call _internal\setenv.bat
     --input-dir "%WORKSPACE%\data_src" ^
     --output-dir "%WORKSPACE%\data_src\aligned" ^
     --detector s3fd ^
-    --output-debug ^
+    --no-output-debug ^
     --force-gpu-idxs 0
 
-rem del "%WORKSPACE%\data_src\*.png" /q
+del "%WORKSPACE%\data_src\*.png" /q
 
 pause
