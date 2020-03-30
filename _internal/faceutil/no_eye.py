e7421287e5d0708f8aff92fdfc7b7e6271251372
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-03-31 02:47:43 +0900

    Add files via upload

---
+++ _internal/faceutil/no_eye.py
@@ -0,0 +1,57 @@
+import sys
+import os
+from os import path
+from pathlib import Path, PureWindowsPath
+from shutil import move
+import cv2
+
+INTERNAL = os.environ['INTERNAL']
+WORKSPACE = os.environ['WORKSPACE']
+
+target_dir = WORKSPACE
+target_dir = os.path.join(target_dir, 'data_src', 'aligned')
+
+file_count = len(os.listdir(target_dir))
+
+print("Checking " + str(file_count) + " files")
+
+no_eye_path = os.path.join(target_dir, 'no_eye')
+
+if not path.isdir(no_eye_path):
+    try:
+        os.mkdir(no_eye_path)
+    except OSError:
+        print("Creation of the directory %s failed" % no_eye_path)
+    else:
+        print("Successfully created the directory %s " % no_eye_path)
+
+face_cascade = cv2.CascadeClassifier(INTERNAL + "/faceutil/haarcascade_frontalface_alt2.xml")
+profile_cascade = cv2.CascadeClassifier(INTERNAL + "/faceutil/haarcascade_profileface.xml")
+eye_cascade = cv2.CascadeClassifier(INTERNAL + "/faceutil/haarcascade_eye.xml")
+#mouth_cascade = cv2.CascadeClassifier(INTERNAL + "/faceutil/haarcascade_mcs_mouth.xml")
+
+for thisFile in os.listdir(target_dir):
+    file_name = os.path.join(target_dir, thisFile)
+    if os.path.isfile(file_name):
+        file_name = os.path.join(target_dir, thisFile)
+
+        img_color = cv2.imread(file_name)
+        img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(
+            img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(45, 45))
+
+        profiles = profile_cascade.detectMultiScale(
+            img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(30, 30))
+
+        eyes = eye_cascade.detectMultiScale(
+            img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(30, 30))
+
+#        mouth = mouth_cascade.detectMultiScale(
+#            img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(30, 30))
+#
+        if (len(faces) == 1 or len(profiles) == 1) and len(eyes) == 0:
+            no_eye_file = os.path.join(no_eye_path, thisFile)
+            if os.path.isfile(file_name):
+                move(
+                    file_name, no_eye_file)
