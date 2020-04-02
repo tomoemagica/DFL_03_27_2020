a4ed35aaf459e236d6fb2e2382064e5d40027d39
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-04-03 00:45:36 +0900

    Add files via upload

--- _internal/faceutil/no_face.py
+++ _internal/faceutil/no_face.py
@@ -27,8 +27,6 @@ if not path.isdir(no_face_path):
 
 face_cascade = cv2.CascadeClassifier(INTERNAL + "/faceutil/haarcascade_frontalface_alt2.xml")
 profile_cascade = cv2.CascadeClassifier(INTERNAL + "/faceutil/haarcascade_profileface.xml")
-eye_cascade = cv2.CascadeClassifier(INTERNAL + "/faceutil/haarcascade_eye.xml")
-mouth_cascade = cv2.CascadeClassifier(INTERNAL + "/faceutil/haarcascade_mcs_mouth.xml")
 
 for thisFile in os.listdir(target_dir):
     file_name = os.path.join(target_dir, thisFile)
@@ -44,13 +42,7 @@ for thisFile in os.listdir(target_dir):
         profiles = profile_cascade.detectMultiScale(
             img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(30, 30))
 
-        eyes = eye_cascade.detectMultiScale(
-            img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(30, 30))
-
-        mouth = mouth_cascade.detectMultiScale(
-            img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(30, 30))
-
-        if len(faces) == 0 and len(profiles) == 0 and len(eyes) == 0 and len(mouth) == 0:
+        if len(faces) == 0 and len(profiles) == 0:
             no_face_file = os.path.join(no_face_path, thisFile)
             if os.path.isfile(file_name):
                 move(
