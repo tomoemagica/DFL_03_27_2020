485867a8d0ca611994b25d0ac746ba709cf6006c
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-04-23 16:11:59 +0900

    Add files via upload

---
+++ m) data_dst Color Correct.bat
@@ -0,0 +1,24 @@
+@echo off
+call _internal\setenv.bat
+
+mkdir "%WORKSPACE%\data_dst" 2>nul
+
+"%PYTHON_EXECUTABLE%" "%DFL_ROOT%\main.py" videoed extract-video ^
+    --input-file "%WORKSPACE%\data_dst.*" ^
+    --output-dir "%WORKSPACE%\data_dst" ^
+    --output-ext "png" ^
+    --fps 0
+
+"%PYTHON_EXECUTABLE%" "%INTERNAL%\faceutil\colcor.py"
+
+ffprobe -i %WORKSPACE%\data_dst.mp4 2>&1 | grep bitrate | gawk -F: '{print $6}' | gawk -F' ' '{print $1}' > tmp.txt
+
+"%PYTHON_EXECUTABLE%" "%DFL_ROOT%\main.py" videoed video-from-sequence ^
+    --input-dir "%WORKSPACE%\data_dst\colcor" ^
+    --output-file "%WORKSPACE%\colcor.mp4" ^
+    --reference-file "%WORKSPACE%\data_dst.*" ^
+    --include-audio < tmp.txt
+
+del tmp.txt
+
+pause
